dfce2490095beaa341f3f4c415f5940ddcf06f3d
Enzo990X <bkanurkin@live.com>
2025-05-05 21:14:29 +0300

    WordsFile created

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,35 @@
-package org.example
+package ktb
+
+import java.io.File
 
 fun main() {
-    println("Hello World!")
+
+    val wordsFile: File = File("words.txt")
+    wordsFile.createNewFile()
+
+    updateDictionary(wordsFile)
+
+    for (line in wordsFile.readLines()) {
+        println(line)
+    }
 }
+
+fun updateDictionary(dictionary: File) {
+    println("Хотите пополнить Ваш словарь?")
+    val enterAnswer = readln()
+
+    if (enterAnswer.equals("да", ignoreCase = true)) addWord(dictionary)
+}
+
+fun addWord(wordsFile: File) {
+
+    do {
+        println("\nВведите новое слово: ")
+        val newWord = getNewWord()
+        wordsFile.appendText("$newWord\n", Charsets.UTF_8)
+
+        println("\nХотите ввести ещё одно слово?")
+    } while (readln().equals("да", ignoreCase = true))
+}
+
+fun getNewWord(): String = readln().lowercase()

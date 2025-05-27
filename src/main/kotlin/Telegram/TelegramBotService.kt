15ccf0f2af62d7566d9856c310e1247be7dfbc3d
Enzo990X <bkanurkin@live.com>
2025-05-27 07:49:52 +0300

    ChatId created

---
+++ src/main/kotlin/Telegram/TelegramBotService.kt
@@ -0,0 +1,44 @@
+package Telegram
+
+import java.net.URI
+import java.net.http.HttpClient
+import java.net.http.HttpRequest
+import java.net.http.HttpResponse
+
+class TelegramBotService(val botToken: String, val chatId: String) {
+
+    fun sendMessage(botToken: String, chatId: String, message: String) {
+
+        try {
+            val encodedMessage = message.replace(" ", "%20")
+            val urlSendMessage =
+                "https://api.telegram.org/bot$botToken/sendMessage?chat_id=$chatId&text=$encodedMessage"
+            val client = HttpClient.newBuilder().build()
+            val request = HttpRequest.newBuilder()
+                .uri(URI.create(urlSendMessage))
+                .build()
+
+            client.send(request, HttpResponse.BodyHandlers.ofString())
+        } catch (e: Exception) {
+            println("Failed to send message: ${e.message}")
+        }
+    }
+
+    fun getUpdates(botToken: String, updateId: Int): String {
+
+        return try {
+            val urlGetUpdates = "https://api.telegram.org/bot$botToken/getUpdates?offset=$updateId&timeout=30"
+            val client = HttpClient.newBuilder().build()
+            val request = HttpRequest.newBuilder()
+                .uri(URI.create(urlGetUpdates))
+                .build()
+
+            val response = client.send(request, HttpResponse.BodyHandlers.ofString())
+            response.body()
+        } catch (e: Exception) {
+            println("Error getting updates: ${e.message}")
+            ""
+        }
+    }
+
+}
